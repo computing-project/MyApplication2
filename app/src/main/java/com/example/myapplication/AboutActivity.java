b16d312554e3f9f89e8c3507ba74754ea343c2ae
rubiksfreak <38412531+rubiksfreak@users.noreply.github.com>
2019-05-06 03:43:24 +1000

    Assesment Time

--- app/src/main/java/com/example/myapplication/AboutActivity.java
+++ app/src/main/java/com/example/myapplication/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Button next = (Button)findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, AssesmentTime.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
