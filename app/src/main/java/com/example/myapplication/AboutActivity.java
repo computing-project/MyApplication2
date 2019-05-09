0aa79a3310d449bad45c526bbdf2c6ea15978842
rubiksfreak <38412531+rubiksfreak@users.noreply.github.com>
2019-05-09 19:50:09 +1000

    About

--- app/src/main/java/com/example/myapplication/AboutActivity.java
+++ app/src/main/java/com/example/myapplication/AboutActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -13,6 +14,15 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Button next = (Button)findViewById(R.id.button);
+        EditText pname=(EditText)findViewById(R.id.pname);
+        EditText sname=(EditText)findViewById(R.id.sname);
+        EditText scode=(EditText)findViewById(R.id.scode);
+        EditText desc=(EditText)findViewById(R.id.sname2);
+        String ProjName=pname.getText().toString();
+        String SubName=sname.getText().toString();
+        String SubCode=scode.getText().toString();
+        String Desc=desc.getText().toString();
+        
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
