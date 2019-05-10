fa424c391d78188351a540536a9a16b2331034db
rubiksfreak <38412531+rubiksfreak@users.noreply.github.com>
2019-05-10 12:43:11 +1000

    Update MarkingCriteria.java

--- app/src/main/java/com/example/myapplication/MarkingCriteria.java
+++ app/src/main/java/com/example/myapplication/MarkingCriteria.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MarkingCriteria extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MarkingCriteria extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marking_criteria);
+        Button next=(Button)findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MarkingCriteria.this, CustomisedGrading.class);
+                startActivity(intent);
+
+            }
+        });
     }
+
 }
