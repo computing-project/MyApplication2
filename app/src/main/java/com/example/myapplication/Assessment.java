f00c5b956c583d43ef2caa00243e8de970a599a8
rubiksfreak <38412531+rubiksfreak@users.noreply.github.com>
2019-05-15 01:27:34 +1000

    Group Assessment

--- app/src/main/java/com/example/myapplication/Assessment.java
+++ app/src/main/java/com/example/myapplication/Assessment.java
@@ -1,7 +1,12 @@
 package com.example.myapplication;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Assessment extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class Assessment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment);
+        ImageButton play= (ImageButton) findViewById(R.id.imageButton2);
+        TextView time=(TextView)findViewById(R.id.textView23) ;
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(30000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        time.setText("seconds remaining: " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        time.setText("done!");
+                    }
+                }.start();
+            }
+        });
     }
 }
